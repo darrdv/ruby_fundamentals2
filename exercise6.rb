61a325f594dc4bdac3b5c23138f492f5aec30524
Darren.DV <darr.dv@gmail.com>
2016-08-10 15:56:52 -0400

    More developments

--- exercise6.rb
+++ exercise6.rb
@@ -14,3 +14,24 @@ def display_grocery_list()
     puts "* #{item}"
   end
 end
+
+#2.
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+
+#output the number of items
+grocery_list.count
+
+#3.
+grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice"]
+
+grocery_list.each do |item|
+  if item == "bananas"
+    puts "You need to pick up bananas."
+  else
+    puts "You don't need to pick up bananas today."
+  end
+end
+
+#4.
+#Display the second item in the list
+grocery_list[1]
